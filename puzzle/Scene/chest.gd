3128a7da81126ecd4ac8ef5376acf2e15948682d
Damian153 <damiancoman11@gmail.com>
2024-04-13 23:48:30 +0300

    nitel

--- puzzle/Scene/chest.gd
+++ puzzle/Scene/chest.gd
@@ -3,11 +3,13 @@ extends StaticBody2D
 @onready var locked = $locked
 @onready var unlocked = $unlocked
 @onready var inv : Inv = preload("res://inv/playerinv.tres")
-@onready var item : InvItem = preload("res://inv/Items/coin.tres")
+@onready var item : InvItem #= preload("res://inv/Items/coin.tres")
+@onready var slot : InvSlot 
 var status = "locked" #unlocked
 var player_in_area = false
 var can_unlock = false
-var poz
+var poz=0
+#var upd = Inv_UI.new()
 
 func _ready():
 	locked.visible = false
@@ -17,7 +19,7 @@ func _process(delta):
 	if player_in_area:
 		if status == "locked":
 			locked.visible = true
-			search()
+			#search()
 			if can_unlock:
 				unlock()
 				
@@ -29,6 +31,7 @@ func _process(delta):
 func _on_area_2d_body_entered(body):
 	if body.has_method("player"):
 		player_in_area = true
+		search()
 
 func _on_area_2d_body_exited(body):
 	if body.has_method("player"):
@@ -38,15 +41,16 @@ func unlock():
 	if Input.is_action_pressed("action"):
 		status = "unlocked"
 		unlocked.visible = true
+		inv.slots[poz].amount -= 1
 		await get_tree().create_timer(1).timeout
 		unlocked.visible = false
-		#remove(inv.slots[)
-
-func remove(slot:InvSlot):
-	slot.amount -= 1
+		
 
 func search():
 	for i in 6:
-		if Global.coin == item.name: #slot.
-			can_unlock = true
-			var poz = i
+		if inv.slots[i].item:
+			if str(inv.slots[i].item.name)== Global.coin:
+				can_unlock = true
+				poz=i
+				print(str(inv.slots[i].item.name))
+				print(str(inv.slots[i].amount))
--- puzzle/Scripts/global.gd
+++ puzzle/Scripts/global.gd
@@ -3,6 +3,7 @@ extends Node
 var run = 3
 var can_collect = false
 var coin :String
+var remove = false
 func _process(delta):
 	if Input.is_action_just_pressed("esc"):
 		get_tree().quit()
